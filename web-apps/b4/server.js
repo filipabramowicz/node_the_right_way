60ab4f0d993f1075683e97a90fba812bf5e88bb6
filipabramowicz <filip.abramowicz@gmail.com>
2014-06-19 01:08:51 +0200

    Web application completed

--- web-apps/b4/server.js
+++ web-apps/b4/server.js
@@ -7,8 +7,14 @@ const
 	redisClient = require('redis').createClient(),
 	RedisStore = require('connect-redis')(session),
 	cookieParser = require('cookie-parser'),
-	passport = require('passport');
+	passport = require('passport'),
+	log = require('npmlog'),
+	json = require('express-json'),
 	GoogleStrategy = require('passport-google').Strategy;
+
+redisClient
+	.on('ready', function() { log.info('REDIS', 'ready'); })
+	.on('error', function(err) { log.error('REDIS', err.message); });
 
 passport.serializeUser(function(user, done){
 	done(null, user.identifier);
@@ -29,6 +35,7 @@ passport.use(new GoogleStrategy({
 
 
 app.use(cookieParser('unguessable'));
+app.use(json());
 app.use(session({ store: new RedisStore({ client: redisClient }), secret: 'unguessable'}));
 app.use(passport.initialize());
 app.use(passport.session());
@@ -52,6 +59,56 @@ app.get('/auth/google/:return?',
 app.get('/auth/logout', function(req, res) {
 	req.logout();
 	res.redirect('/');
+});
+
+const authed = function(req, res, next) {
+	if (req.isAuthenticated()) {
+		return next();
+	} else if (redisClient.ready) {
+		res.json(402, {
+			error: 'forbidden',
+			reason: 'not_authenticated'
+		});
+	} else {
+		res.json(503, { 
+			error: 'service_unavailable',
+			reason: 'authentication_unavilable'
+		});
+	}
+}
+
+app.get('/api/user', authed, function(req, res){
+	res.json(req.user);
+});
+
+app.get('/api/user/bundles', authed, function(req, res) {
+	let userURL = config.b4db + encodeURIComponent(req.user.identifier);
+	request(userUrl, function(err, couchRes, body) {
+		if (err) {
+			res.json(502, { error: "bad_gateway", reason: err.code });
+		} else if (couchRes.statusCode === 200) {
+			res.json(JSON.parse(body).bundles || {} );
+		} else
+		res.send(couchRes,statusCode, body);
+	});
+});
+
+app.put('/api/user/bundles', [authed, json()], function(req, res) {
+	let userUrl = config.b4db + encodeURIComponent(req.user.identifier);
+	request(userURL, function(err, couchRes, body) {
+		if (err) {
+			res.json(502, { error: "bad_gateway", reason: err.code });
+		} else if (couchRes.statusCode === 200) {
+			let user = JSON.parse(body);
+			user.bundles = req.body;
+			request.put( {url: userURL, json: user} ).pipe(res);
+		} else if (couchRes.statusCode === 404) {
+			let user = { bundles: req.body };
+			request.put( {url: userURL, json: user} ).pipe(res);
+		} else {
+			res.send(couchRes.statusCode, body)
+		}
+	});
 });
 
 app.get('/api/:name', function(req,res){
